3443abaf5c2186006550ab9bd3972b69f4e0d90c
Danila Poyarkov <dev@dannote.net>
2026-03-17 17:50:56 +0300

    Bump v0.5.2 — fix default keyword in bundled IIFE minification

--- checksum-Elixir.OXC.Native.exs
+++ checksum-Elixir.OXC.Native.exs
@@ -1,7 +1,1 @@
-%{
-  "liboxc_ex_nif-v0.5.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2d258b052d0072e57b98937f9774e8d2e7f2695c1478a6c0bd900d98c06708c4",
-  "liboxc_ex_nif-v0.5.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9062e0709ddf686a46bc81c1c547d2a939ddaadcbc49a0692fcc8479acec0586",
-  "liboxc_ex_nif-v0.5.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:9c42dff7b0f3c2acc9d530ae3debb333e1c1234efe9acd1d98ef6203d19f20be",
-  "liboxc_ex_nif-v0.5.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b981bca9284e8cb6e46027ac0de25cf81da59834165eec6fb60329ed14692b33",
-  "liboxc_ex_nif-v0.5.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:05c67b9ad264b6f0fe1556c7561236306b98113bcc7c7fea5fd6eac0fa1e191d",
-}
+%{}
